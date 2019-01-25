b184d9ba3e579cde322dae9575b9dfbc89cf2be9
yhydev <31916483+yhydev@users.noreply.github.com>
2019-01-25 22:07:09 +0800

    update spider-run.py, use threadpool.

--- spider-run.py
+++ spider-run.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import loginit, logging, config
 from spider import Spider,Parser
+from concurrent.futures import ThreadPoolExecutor
 
 
+MAX_WORKERS = 30
 
-
+pool = ThreadPoolExecutor(max_workers = MAX_WORKERS)
 
 for op in config.spiderConfig:
     parser = Parser(op["proxyre"], op["ipre"], op["portre"], op["proxyre"])
     spider = Spider(op["urls"], parser)
-    proxys = spider.getProxys()
+    #proxys = spider.getProxys()
+
+    proxys = pool.submit(spider.getProxys).result()
 
     for proxy in proxys:
         logging.info("%s:%s", proxy.ip, proxy.port)
